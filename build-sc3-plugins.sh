de959cb4ce1d30d2997cc551e6f6db4477e347d0
Mauro <mauro@sdf.org>
2017-03-25 02:58:53 -0300

    added sc3-plugins

---
+++ build-sc3-plugins.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+###
+#
+# Variables
+#
+INSTALL_DIR=/tmp
+SC3PLUGINS_DIR=$INSTALL_DIR/sc3-plugins
+SC3PLUGINS_BUILD_DIR=$SC3PLUGINS_DIR/build
+
+###
+#
+# Update
+#
+sudo apt update
+
+###
+#
+# Install building dependencies
+#
+sudo apt install -yq \
+         build-essential pkg-config cmake subversion git \
+         libjack-jackd2-dev libudev-dev libsndfile1-dev libasound2-dev \
+         libavahi-client-dev libicu-dev libreadline6-dev libfftw3-dev \
+         libxt-dev libcwiid-dev libqt5webkit5-dev libqt5sensors5-dev \
+         qt5-default qt5-qmake qttools5-dev qttools5-dev-tools \
+         qtdeclarative5-dev qtpositioning5-dev
+
+###
+#
+# Download and build SC
+#
+mkdir -p $SC3PLUGINS_DIR
+
+# Download SC Source
+git clone --recursive https://github.com/supercollider/sc3-plugins.git $SC3PLUGINS_DIR
+
+# Create the the directory where SC will be built
+mkdir -p $SC3PLUGINS_BUILD_DIR
+
+# And build it.
+cd $SC3PLUGINS_BUILD_DIR && \
+    cmake -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu/ \
+          -DCMAKE_INSTALL_PREFIX=/usr/local \
+          -DSC_PATH=/tmp/supercollider/ \
+          -DBUILD_TESTING=OFF -DQUARKS=ON -DSUPERNOVA=ON .. && \
+    make -j4 && \
+    sudo make install && \
+    sudo ldconfig
